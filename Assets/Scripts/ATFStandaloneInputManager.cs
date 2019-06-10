dd1e93f649da708eaed4af6702e33184c5f33dfc
Oleg <oabedrin@stud.kpfu.ru>
2019-06-10 13:23:04 +0300

    Initialize proj

---
+++ Assets/Scripts/ATFStandaloneInputManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ATFStandaloneInputManager : StandaloneInputModule
+{
+    protected override void Awake()
+    {
+        base.Awake();
+        m_InputOverride = gameObject.AddComponent<ATFInput>();
+    }
+}
